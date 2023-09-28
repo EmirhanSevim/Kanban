9d7e4413bf78faf9ef9daf90430979ba2010eda0
EmirhanSevim <emirhansevim182@gmail.com>
2023-09-28 16:01:44 +0300

    düzgün

--- mycanban/src/components/Board.vue
+++ mycanban/src/components/Board.vue
@@ -2,16 +2,13 @@
   <div>
     <nav class="navbar">
       <div class="navbar-title" v-for="boardItem in boardList" :key="boardItem.id">
-        <input type="text" v-model="boardItem.baslik" @input="updatePageTitle" @blur="updatePanoName(boardItem)" />
+        <input class="baslikinput" type="text" v-model="boardItem.baslik" @input="updatePageTitle" @blur="updatePanoName(boardItem)" />
       </div>
     </nav>
 
     <div class="board">
       <div class="list" v-for="(item, index) in panoList" :key="index">
-        <!-- <div>
-          <Board :listBaslik="pageTitle" />
-        </div> -->
-        <!-- <h3 @click="logBaslik(item.listBaslik)">{{ item.listBaslik }}</h3> -->
+        <h3 @click="logBaslik(item.listBaslik)">{{ item.listBaslik }}</h3>
         <div class="card" v-for="(cardItem, cardIndex) in item.kartItems" :key="cardIndex">
           {{ cardItem.baslik }}
         </div>
@@ -32,13 +29,6 @@ import { ref, onMounted, defineComponent } from 'vue';
 import axios from 'axios';
 
 export default defineComponent({
-  // props: {
-  //   listBaslik: {
-  //     type: String,
-  //     required: true,
-  //   },
-  // },
-
   setup() {
     const panoList = ref([]);
     const boardList = ref([]);
@@ -72,40 +62,13 @@ export default defineComponent({
       console.log('Tıklanan Başlık:', baslik);
     };
 
-    // const ekleKart = (item) => {
-
-    //   if (item && item.kartItems) {
-    //     const yeniListe = {
-    //       listBaslik: 'Yeni Liste',
-    //       kartItems: [],
-    //     };
-    //     item.kartItems.push(yeniListe);
-    //   } else {
-    //     console.error('item veya item.kartItems tanımlanmamış veya undefined.');
-    //   }
-    // };
-
-    const ekleKart = (kartAdi) => {
+    const ekleKart = (kartAdi: string) => {
       const yeniListe = {
         listBaslik: 'Yeni Liste',
         kartItems: [],
       };
       item.kartItems.push(kartAdi);
     };
-
-    // const ekleListe = (item) => {
-    //   const yeniListeBaslik = 'Yeni Liste';
-
-    //   axios
-    //     .post('https://localhost:44355/api/app/kart-item-detay-services', { listBaslik: yeniListeBaslik })
-    //     .then((response) => {
-    //       const yeniListe = response.data;
-    //       item.lists.push(yeniListe);
-    //     })
-    //     .catch((error) => {
-    //       console.error('Yeni liste eklerken hata:', error);
-    //     });
-    // };
 
     const ekleListe = (param: string) => {
       const yeniListe = {
@@ -117,23 +80,6 @@ export default defineComponent({
         .post('https://localhost:44355/api/app/list-app-services', yeniListe)
         .then((response) => {
           console.log('response ', response);
-          // // Sunucudan dönen yanıtı işleyin (örneğin, yeni board ID'sini alabilirsiniz)
-
-          // // Yeni board verisine sunucudan gelen ID'yi ekleyin
-
-          // // panoList'e yeni board'i ekleyin
-
-          // // panoItemId ile ilgili güncelleme işlemi
-          // axios
-          //   .put(`https://localhost:44355/api/app/list-app-services/${yeniListe.panoItemId}`, {
-          //     boardId: yeniBoardID, // panoItemId'yi güncellemenin yolu ve verisi
-          //   })
-          //   .then(() => {
-          //     console.log('panoItemId güncellendi.');
-          //   })
-          //   .catch((error) => {
-          //     console.error('panoItemId güncelleme hatası:', error);
-          //   });
         })
         .catch((error) => {
           console.error('Yeni board eklerken hata:', error);
@@ -163,8 +109,6 @@ export default defineComponent({
     },
   },
 });
-//https://localhost:44355/api/app/pano-app-services/3a0de902-d95b-503d-1ff6-bc67ae0de648
-//https://localhost:44355/api/app/pano-app-services/3a0de902-d95b-503d-1ff6-bc67ae0de648
 </script>
 
 <style>
@@ -232,36 +176,50 @@ export default defineComponent({
 body {
   background: linear-gradient(135deg, rgba(57, 221, 106, 0.767), rgba(16, 204, 141, 0.664), rgba(9, 64, 146, 0.685), rgba(12, 48, 207, 0.473));
   background-size: 400% 400%;
-  /* animation: gradientAnimation 20s infinite; */
+  animation: gradientAnimation 5s infinite;
 }
-/*
+
 @keyframes gradientAnimation {
   0% {
-    background-position: 0% 100%;
+    background-position: 300% 50%;
   }
   50% {
-    background-position: 100% 50%;
+    background-position: 50% 100%;
   }
   100% {
-    background-position: 0% 50%;
-  } */
-/* } */
+    background-position: 100% 300%;
+  }
+}
+
+.baslikinput {
+  margin-top: 2px;
+  width: 222px;
+  border: none;
+  outline: none;
+  background-color: transparent;
+  height: 20px;
+  padding: 3px;
+  border-radius: 3px;
+  align-items: flex-start;
+  cursor: pointer;
+  transition: background-color 1s ease;
+  font-size: 20px;
+}
 
 input[type='text'] {
   margin-top: 2px;
   width: 222px;
+  margin-left: -10px;
   height: 20px;
   padding: 3px;
-  /* border: 1px solid #ccc; */
   border-radius: 3px;
   align-items: flex-start;
-  background-color: #5c58586b; /* Saydamlık ekleyin */
-  cursor: pointer; /* İmleci el işareti yapın */
-  transition: background-color 1s ease; /* Geçiş efekti ekleyin */
+  cursor: pointer;
+  transition: background-color 1s ease;
 }
 
 input[type='text']:hover {
-  background-color: rgba(240, 240, 240, 1); /* İmleç üstüne geldiğinde tamamen görünür yapın */
+  background-color: rgba(240, 240, 240, 1);
 }
 
 .ekle-button-container {
@@ -272,6 +230,8 @@ input[type='text']:hover {
 
 .ekle-button {
   background-color: #ccc8e093;
+  margin-left: -10px;
+  margin-top: 5px;
   color: #fff;
   padding: 5px 20px;
   border-radius: 5px;
@@ -291,6 +251,7 @@ input[type='text']:hover {
 .ekle-board {
   background-color: #ccc8e093;
   color: #fff;
+  margin-right: 10 px;
   padding: 5px 4px;
   border: center;
   border-radius: 5px;
